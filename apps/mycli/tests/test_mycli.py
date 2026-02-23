021bf865e8c0d9c881a49efd47f156c4387cdca1
Gabriel Legault <gabriel.legault@goto.com>
2026-02-23 16:40:47 -0500

    Add nx

--- apps/mycli/tests/test_mycli.py
+++ apps/mycli/tests/test_mycli.py
@@ -1,5 +1,15 @@
+from contextlib import redirect_stdout
+from io import StringIO
+from types import SimpleNamespace
+from unittest.mock import patch
+
 from postmodern.mycli import cli
 
 
-def test_main():
-    cli.main()
+def test_main() -> None:
+    stdout = StringIO()
+    with patch("urllib3.request", return_value=SimpleNamespace(status=200)):
+        with redirect_stdout(stdout):
+            cli.main()
+
+    assert stdout.getvalue().strip() == "200"
